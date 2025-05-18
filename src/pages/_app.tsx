9a480141e43fbdabb2b700d7bc994e3410186e0a
stewart-lore <stewart@lore.xyz>
2025-05-18 07:37:40 -0700

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { sdk } from "@farcaster/frame-sdk";
+
+sdk.on("frameAdded", (user) => {
+  console.log("user.loggedIn", user);
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
